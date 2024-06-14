ae55da7c4149802d82a88856bc461db611b4f26a
Samantha Remmey <sqr8ap@virginia.edu>
2024-06-14 15:26:48 -0400

    adding all

--- Plots.R
+++ Plots.R
@@ -2,5 +2,21 @@
 
 library(tidyverse)
 
+# Normal
+ggplot(data.frame(n=rnorm(1000)), aes(x=n))+geom_histogram()+theme_classic()
+
+# Poisson
+ggplot(data.frame(n=rpois(1000,10)), aes(x=n))+geom_histogram(bins=15)+theme_classic()
+
+# Binomial
+ggplot(data.frame(n=rbinom(1000, size=100, prob=0.5)), aes(x=n))+geom_histogram()+theme_classic()
+
+
+
+
 # class data (facebook)
+
+
+
+
 
