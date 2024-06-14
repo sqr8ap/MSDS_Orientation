f64d685f9095f48c610d670819b068e05dbaa4d6
Samantha Remmey <sqr8ap@virginia.edu>
2024-06-14 14:59:14 -0400

    adding Euler.R

---
+++ Euler.R
@@ -0,0 +1,19 @@
+# Euler problems 1-3
+
+## 1
+my_list <- list()
+for (x in 1:1000){
+  if (x %% 3 == 0){
+    append(my_list, x)}
+  else if (x %% 5 == 0){
+    append(my_list, x)}
+}
+
+## 2
+
+
+
+## 3
+
+
+
