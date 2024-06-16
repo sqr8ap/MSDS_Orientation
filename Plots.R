9159c2cd7375b95e43065a84b0df718c0f815a32
Sam Remmey <123048052+sqr8ap@users.noreply.github.com>
2024-06-15 23:09:39 -0400

    Update Plots.R

--- Plots.R
+++ Plots.R
@@ -12,9 +12,14 @@ ggplot(data.frame(n=rpois(1000,10)), aes(x=n))+geom_histogram(bins=15)+theme_cla
 ggplot(data.frame(n=rbinom(1000, size=100, prob=0.5)), aes(x=n))+geom_histogram()+theme_classic()
 
 
+# Hardware data
+dat <- read.csv("/Users/Samantha/Downloads/MSDS-Orientation-Computer-Survey(in).csv")
+ggplot(dat, aes(x=CPU.Cycle.Rate..in.GHz.))+geom_histogram()
+ggplot(dat, aes(x=CPU.Number.of.Cores..int.))+geom_histogram()
+ggplot(dat, aes(x=RAM..in.GB.))+geom_histogram()
+ggplot(dat, aes(x=Hard.Drive.Size..in.GB.))+geom_histogram()
+ggplot(dat, aes(x=GPU.CUDA.Number.of.Cores..int.))+geom_histogram()
 
-
-# class data (facebook)
 
 
 
